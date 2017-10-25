6209175eb577c01306600f2e7f3a2caec76a424b
brokyo <alexcarusillo@gmail.com>
2017-10-24 23:43:37 -0400

    list of all saved music boxes

---
+++ pages/all.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <h1>All Music Boxes</h1>
+    <div v-for="box in boxArray">
+      <h4><router-link :to="{ name: 'musicboxes-id', params: { id: box }}">{{allBoxes[box].meta.title}}</router-link></h4>
+      <h6>{{allBoxes[box].meta.knobtwister}}</h6>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from '../plugins/axios.js'
+
+export default {
+
+  name: 'all',
+  asyncData () {
+    return axios.get('musicboxes.json')
+    .then((res) => {
+      return {
+        boxArray: Object.keys(res.data),
+        allBoxes: res.data
+      }
+    })
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
